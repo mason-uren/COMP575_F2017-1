c01655060f2efa2ff86e3472ee65ab25f5a831c6
mason-uren <mason.uren600@myci.csuci.edu>
2017-11-28 22:00:40 -0800

    Fixed conflicts

--- src/mobility/src/mobility.cpp
+++ src/mobility/src/mobility.cpp
@@ -311,13 +311,9 @@ void headingHandler(const std_msgs::Float64MultiArray::ConstPtr &message) {
     end = NULL; // clear pointer
 
 //    Call localPose
-<<<<<<< HEAD
     std_msgs::String lP = localPose((int)  message->data[0]);
     rover_hash[current_rover].avg_local_pose = std::strtod(lP.data.c_str(), &end);
-    end = NULL; // clear pointer
-=======
-
->>>>>>> 6ff87806b94f5d0b532c50375c9aaf278b546bff
+    end = NULL; // clear pointers
 }
 
 std_msgs::String globalHeading (){
